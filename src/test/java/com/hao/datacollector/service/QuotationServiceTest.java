ab03a6bed3abd2fa7fddd6c3ceb4a33816cc0eeb
hli <lihao836184425gmail.com>
2025-07-18 17:33:36 +0800

    tb_quotation_history_trend_202504

--- src/test/java/com/hao/datacollector/service/QuotationServiceTest.java
+++ src/test/java/com/hao/datacollector/service/QuotationServiceTest.java
@@ -43,7 +43,8 @@ class QuotationServiceTest {
         List<String> allWindCodeList = new ArrayList<>(StockCache.allWindCode);
         List<String> yearTradeDateList = DateUtil.formatLocalDateList(DateCache.CurrentYearTradeDateList, DateTimeFormatConstant.EIGHT_DIGIT_DATE_FORMAT);
         //从当年已转档的最大日期(包含),并且剔除最大日期已经转档过的windCode,继续开始转档
-        String maxEndDate = quotationMapper.getMaxHistoryTrendEndDate("2025");
+//        String maxEndDate = quotationMapper.getMaxHistoryTrendEndDate("2025");
+        String maxEndDate = "20250401";
         List<String> completedWindCodes = quotationMapper.getCompletedWindCodes(maxEndDate);
         int tradeDateIndexOf = yearTradeDateList.indexOf(maxEndDate);
         int batchSize = 100;
@@ -66,6 +67,9 @@ class QuotationServiceTest {
     private void transferOneDay(List<String> yearTradeDateList, List<String> windCodes, int batchSize) {
         int totalSize = windCodes.size();
         for (String tradeDate : yearTradeDateList) {
+            if (tradeDate.equals("20250501")) {
+                throw new RuntimeException("20250501!!!!");
+            }
             for (int i = 0; i < totalSize; i += batchSize) {
                 List<String> subList = windCodes.subList(i, Math.min(i + batchSize, totalSize));
                 String windCodeStr = String.join(",", subList);
