145b281adf2fbe44b44772e610e64e554b2b6fba
hli <lihao836184425gmail.com>
2025-07-22 19:11:13 +0800

    202403

---
+++ src/main/java/com/hao/datacollector/web/controller/StockProfileController.java
@@ -0,0 +1,17 @@
+package com.hao.datacollector.web.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hli
+ * @program: datacollector
+ * @Date 2025-07-22 19:08:20
+ * @description: 个股资料相关接口
+ */
+@Tag(name = "个股资料相关接口")
+@RestController
+@RequestMapping("stock_profile")
+public class StockProfileController {
+}
